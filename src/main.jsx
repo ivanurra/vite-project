8d81fe02316aba6f47cb1f28cd0e9cc84aae65e3
Ivan Urra <ivan.urra.wd@gmail.com>
2022-06-26 17:48:49 +0200

    start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
