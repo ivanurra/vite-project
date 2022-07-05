0bf30b7774b3b3aea665524319289f9977f02c5e
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-05 17:09:57 +0200

    wip

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
-export const CounterApp = ({ value }) => {
+export const CounterApp = () => {
+  const [counter, setCounter] = useState(0);
+  const sum = () => {
+    return setCounter(counter + 1);
+  };
+
+  const rest = () => {
+    return setCounter(counter - 1);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2> {counter} </h2>
+      <button onClick={sum}>+1</button>
+      <button onClick={rest}>-1</button>
+      <div>{counter < 0 ? "Under zero 😖" : "Go, go! 🚀"}</div>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <FirstApp/> */}
-    <CounterApp value={1} />
+    <CounterApp />
   </React.StrictMode>
 );
