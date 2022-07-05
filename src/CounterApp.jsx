d7db03947c2c4e9f0a7582fa64c07163a1765dc6
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-05 17:17:05 +0200

    wip

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,19 +4,24 @@ import PropTypes from "prop-types";
 export const CounterApp = () => {
   const [counter, setCounter] = useState(0);
   const up = () => {
-    return setCounter(counter + 1);
+    setCounter(counter + 1);
   };
 
   const down = () => {
-    return setCounter(counter - 1);
+    setCounter(counter - 1);
+  };
+
+  const reset = () => {
+    setCounter(0);
   };
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {counter} </h2>
+      <h1> {counter} </h1>
       <button onClick={up}>+1</button>
       <button onClick={down}>-1</button>
+      <button onClick={reset}>Reset</button>
       <div>{counter < 0 ? "Under zero 😖" : "Go, go! 🚀"}</div>
     </>
   );
