6581719ba72fb7863a3bdef92cd60f0b9675fd1d
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-05 16:16:37 +0200

    WIP

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FirstApp } from "./FirstApp";
+import { CounterApp } from "./CounterApp";
+// import { FirstApp } from "./FirstApp";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp/>
+    {/* <FirstApp/> */}
+    <CounterApp value={1} />
   </React.StrictMode>
 );
